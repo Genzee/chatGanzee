3c6c52feac2259c651fe9952c20f08c96fa119b6
Min-Suk Kim <mspeepoo@gmail.com>
2023-02-16 21:37:55 +0900

    example prompt - readline 모듈 이용

--- example/example.js
+++ example/example.js
@@ -1,1 +1,23 @@
-console.log('start example')
+console.log('This is Example Script')
+
+var readline = require('readline')
+
+var r = readline.createInterface({
+	input:process.stdin,
+	output:process.stdout
+});
+
+r.setPrompt('> ');
+r.prompt();
+
+r.on('line', function(line){
+	if (line == 'exit') {
+		r.close();
+	}
+	console.log(line);
+	r.prompt()
+});
+
+r.on('close', function() {
+	process.exit();
+});
